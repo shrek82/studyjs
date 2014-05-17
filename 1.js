f251133dc8c39ea0bb9d7ba8550a47e0a9caca7c
shrek82 <37294812@qq.com>
2014-05-17 19:04:06 +0800

    add 1

---
+++ 1.js
@@ -0,0 +1,21 @@
+console.log(typeof 123);
+console.log(typeof window);
+
++ function zjg(age) {
+    console.log(age);
+}(30), + function lhm(age) {
+    console.log(age);
+}(26);
+
+var cat = function() {};
+
+cat.name = function() {
+    return 'zjg';
+}
+
+cat.age = function() {
+    return 30;
+}
+
+console.log(cat);
+console.log(1ls23);
